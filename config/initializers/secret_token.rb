65e123c1d50fa09e0c7fbd8bbc05ffb897d791f8
Michael Xu <mxu@ncsu.edu>
2016-02-02 23:01:27 -0500

    adding a new project. feel free to remove everything. most of this was auto-generated by rubymine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Csc517Program1::Application.config.secret_key_base = '89d6ac76b43148dbb18314ddee8f32a0ad1e1518bbe9a6ea342b244e51e4b90525eb5611da91307383602eb376a6e5ea66feba12a6d2e0fa62ebfb0d8a10943e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Csc517Program1::Application.config.session_store :cookie_store, key: '_csc517_program1_session'
