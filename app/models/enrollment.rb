a3d21cbe8712e878920d1de8a1202a58e875eb2e
Brandon Hagar <bghagar@ncsu.edu>
2016-02-15 22:31:41 -0500

    enrollment has and belongs to many courses too

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,6 +1,6 @@
 class Enrollment < ActiveRecord::Base
   has_and_belongs_to_many :users
-  has_many :courses
+  has_and_belongs_to_many :courses
 
   validates_uniqueness_of :user_id, :scope => :course_id
 end
