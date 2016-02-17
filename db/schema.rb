4dddab79b15054d9de67bf6253de5130f98a9eb1
Brandon Hagar <bghagar@ncsu.edu>
2016-02-17 15:30:22 -0500

    enrollment has and belongs to many courses too

--- db/schema.rb
+++ db/schema.rb
@@ -25,17 +25,14 @@ ActiveRecord::Schema.define(version: 20160216075524) do
   end
 
   create_table "courses_users", id: false, force: true do |t|
-    t.integer "user_id"
     t.integer "course_id"
-    t.boolean "approve"
-    t.boolean "deny"
-    t.integer "grade"
-
+    t.integer "user_id"
   end
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "password"
     t.boolean  "is_student"
     t.boolean  "is_admin"
     t.boolean  "is_instructor"
