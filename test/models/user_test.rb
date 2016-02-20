37adeaa10fc37d43cf7df8e8cee0d63c50ceb14c
Michael Xu <mxu@ncsu.edu>
2016-02-20 16:42:57 -0500

    added some tests. most of them fail though because I'm not sure how to use the users class.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do #this test should always return true. testing the test framework.
+     assert true
+  end
+
+  test "create a user improper parameters" do
+    assert_raise (StandardError){
+      tempuser1 = User.new("name","email@email.com",true,false,false,nil)
+    }
+  end
+
+  test "two users one email address" do
+    assert_raise (StandardError){
+      tempuser1 = User.new("name","email@email.com",true,false,false,nil)
+      tempuser2 = User.new("name","email@email.com",true,false,false,nil)
+    }
+  end
+
 end
