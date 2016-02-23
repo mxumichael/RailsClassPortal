ee370e7337894a0e872b2d767892e0ab7e97793f
Brandon Hagar <bghagar@ncsu.edu>
2016-02-23 13:57:11 -0500

    fixes #21 added jquery turbolinks and made sure links load after resources

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,13 +25,7 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    if (params[:user_role] == 'admin')
-      @user = Admin.new(user_params)
-    elsif (params[:user_role] == 'instructor')
-      @user = Instructor.new(user_params)
-    else
-      @user = Student.new(user_params)
-    end
+    @user = User.new(user_params)
     respond_to do |format|
       if @user.save
         if !logged_in?
@@ -85,7 +79,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :user_role)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :type)
     end
 
   def enroll
--- app/views/users/index.json.jbuilder
+++ app/views/users/index.json.jbuilder
@@ -1,4 +1,1 @@
-json.array!(@users) do |user|
-  json.extract! user, :id, :name, :email, :password, :is_student, :is_admin, :is_instructor
-  json.url user_url(user, format: :json)
-end
+
